ea80987eb71759bc71c970e04ade03ad8e550567
Amit0129 <amitkumarnayak40@gmail.com>
2022-12-16 22:44:32 +0530

    Validate FirstName Starts With Capital letter And Has Minimum Three characters

---
+++ UserRegistration/Pattern.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class Pattern
+    {
+        public bool Validater(string inputString, string pattern)
+        {
+            return Regex.IsMatch(inputString, pattern);
+        }
+        public void FirstNameCheck()
+        {
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("Enter First Name");
+                string firstName = Console.ReadLine();
+                string fstNamePattern = "^[A-Z][A-Za-z]{2,}$";
+                if (Validater(firstName, fstNamePattern))
+                {
+                    Console.WriteLine($"The First Name of The Person is {firstName}");
+                    check = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid FirstName Where first letter must be in capital and it contain minimun three charectar");
+                }
+            }
+        }
+
+    }
+}
--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome to User Registration Program");
+
+            Pattern patternCheck = new Pattern();
+
+            Console.WriteLine("Registration of First name");
+            patternCheck.FirstNameCheck();
         }
     }
 }
